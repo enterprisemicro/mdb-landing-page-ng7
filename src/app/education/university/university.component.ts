a38de3307bad6f0d20f6d98f7efa775c6a5d3f81
mcgrady chen <mcgrady_chen@trendmicro.com>
2019-06-15 11:25:58 +0800

    add university template support

--- src/app/education/university/university.component.ts
+++ src/app/education/university/university.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-university',
   templateUrl: './university.component.html',
-  styleUrls: ['./university.component.scss']
+  styleUrls: ['./university.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class UniversityComponent implements OnInit {
 
